368879b46525719bce8658ecc1a38fe1981a9dbc
ljtbautista <46353764+ljtbautista@users.noreply.github.com>
2020-05-13 16:20:26 -0500

    Added function stuff

--- AzureFunctionDemo/GithubMonitorApp/GithubMonitor.cs
+++ AzureFunctionDemo/GithubMonitorApp/GithubMonitor.cs
@@ -17,17 +17,15 @@ namespace GithubMonitorApp
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
+            log.LogInformation("Our Github Monitor processed and action.");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            //do something with data here
+            log.LogInformation(requestBody);
+
+            return new OkResult();
         }
     }
 }
